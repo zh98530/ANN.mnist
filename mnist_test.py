a3b79274d7b083e940b3010dc351e5146c9f1c24
zh98530 <39448526+zh98530@users.noreply.github.com>
2018-12-02 17:44:04 +0800

    Update mnist_test.py

--- mnist_test.py
+++ mnist_test.py
@@ -1,28 +1,40 @@
 #coding:utf-8
-
+import time
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist_forward
+import mnist_backward
+TEST_INTERVAL_SECS = 5
 
-#照片为28*28像素，数据输入量为784,输出显示为0-9这十个数字，隐藏层节点个数为500
-INPUT_NODE = 784
-OUTPUT_NODE = 10
-LAYER1_NODE = 500
+def test(mnist):
+	with tf.Graph().as_default() as g:
+		x = tf.placeholder(tf.float32,[None,mnist_forward.INPUT_NODE])
+		y_ = tf.placeholder(tf.float32,[None,mnist_forward.OUTPUT_NODE])
+		y = mnist_forward.forward(x,None)
 
-def get_weight(shape,regularizer):
-	w = tf.Variable(tf.random_normal(shape),dtype=tf.float32)
-	if regularizer != None: 
-		tf.add_to_collection('losses',tf.contrib.layers.l2_regularizer(regularizer)(w))
-	return w
+		ema = tf.train.ExponentialMovingAverage(mnist_forward)
+		ema_restore = ema.variables_to_restore()
+		saver = tf.train.Saver(ema_restore)
 
-def get_bias(shape):
-	b = tf.Variable(tf.constant(0.01,shape=shape))
-	return b
-
-def forward(x,regularizer):
-	w1 = get_weight([INPUT_NODE,LAYER1_NODE],regularizer)
-	b1 = get_bias([LAYER1_NODE])
-	y1 = tf.nn.relu(tf.matmul(x,w1) + b1)
+		correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+		
+		while True:
+			with tf.Session() as sess:
+				ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
+				if ckpt and ckpt.model_checkpoint_path:
+					saver.restore(sess,ckpt.model_checkpoint_path)
+					global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+					accuracy_score = sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels})
+					print("After %s training step(s), test accuracy = %g." %(global_step,accuracy_score))
+				else:
+					print('No checkpoints file found')
+					return
+			time.sleep(TEST_INTERVAL_SECS)
 	
-	w2 = get_weight([LAYER1_NODE,OUTPUT_NODE],regularizer)
-	b2 = get_bias([OUTPUT_NODE])
-	y = tf.matmul(y1,w2) + b2
-	return y
+def main():
+	mnist = input_data.read_data_sets("./data/",one_hot=True)
+	test(mnist)
+	
+if __name__ == '__main__':
+	main()
