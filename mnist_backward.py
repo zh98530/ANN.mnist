1f65ca8b2d3ad0bab6ff9252d44f826b3d524283
zh98530 <39448526+zh98530@users.noreply.github.com>
2018-12-05 20:50:19 +0800

    Update mnist_backward.py

--- mnist_backward.py
+++ mnist_backward.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import mnist_forward
@@ -18,24 +19,29 @@ def backward(mnist):
 	y = mnist_forward.forward(x,REGULARIZER)
 	global_step = tf.Variable(0,trainable=False)
 	
+	#前向传播输出经过softmax函数，以获得输出分类的概率分布，与标准答案求出交叉熵，加上正则化权重得损失函数
 	ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,labels=tf.argmax(y_,1))
 	cem = tf.reduce_mean(ce)
 	loss = cem + tf.add_n(tf.get_collection("losses"))
 	
+	#学习率定为指数衰减型
 	learning_rate = tf.train.exponential_decay(
 		LEARNING_RATE_BASE,
 		global_step,
 		mnist.train.num_examples/BATCH_SIZE,
 		LEARNING_RATE_DECAY,
 		staircase=True)
-		
+	
+	#采取梯度下降优化	
 	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
 	
+	#滑动平均更新新的神经网络参数，保留上一次的参数影响，给予其合适的权重影响新参数
 	ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
 	ema_op = ema.apply(tf.trainable_variables())
 	with tf.control_dependencies([train_step,ema_op]):
 		train_op = tf.no_op(name = 'train')
-		
+	
+	#实例化saver对象	
 	saver = tf.train.Saver()
 	
 	with tf.Session() as sess:
@@ -43,11 +49,11 @@ def backward(mnist):
 		sess.run(init_op)
 		
 		for i in range(STEPS):
-			xs,ys = mnist.train.next_batch(BATCH_SIZE)
-			_,loss_value,step = sess.run([train_op,loss,global_step],feed_dict={x:xs,y_:ys})
+			xs,ys = mnist.train.next_batch(BATCH_SIZE)#训练参数
+			_,loss_value,step = sess.run([train_op,loss,global_step],feed_dict={x:xs,y_:ys})#参数喂入得到相应的评估参数
 			if i % 1000 == 0:
 				print("After %d training step(s),loss on training batch is %g." %(step,loss_value)) 
-				saver.save(sess,os.path.join(MODEL_SAVE_PATH,MODEL_NAME),global_step=global_step)
+				saver.save(sess,os.path.join(MODEL_SAVE_PATH,MODEL_NAME),global_step=global_step)#将当前sess保存至指定路径，并命名mnist_model_(global_step)
 
 def main():
 	mnist = input_data.read_data_sets("./data/",one_hot=True)
